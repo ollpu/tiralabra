79475d70b4c6a277740a3fe0298b672614ecf4d0
Roope Salmi <rpsalmi@gmail.com>
2021-04-25 12:13:01 +0300

    Esilaske twiddle-kertoimet
    Suorituskyky parani noin 65%.

--- src/fft.rs
+++ src/fft.rs
@@ -7,13 +7,22 @@ use crate::math::*;
 /// computed in the main routine.
 pub struct Prepared {
     size: usize,
+    twiddle_factors: Vec<Complex>,
 }
 
 impl Prepared {
     /// Prepare FFT. Size has to be a power of two.
     pub fn new(size: usize) -> Self {
         assert!(size.count_ones() == 1);
-        Prepared { size }
+        let half_size = size / 2;
+        let half_size_inverse = 1. / half_size as Num;
+        let twiddle_factors = (0..half_size).map(|i| {
+            Complex::euler(-(i as Num) * half_size_inverse * PI)
+        }).collect();
+        Prepared {
+            size,
+            twiddle_factors,
+        }
     }
 
     /// Perform the transform. The size of the array has to be the same as what
@@ -30,11 +39,15 @@ impl Prepared {
         // The "butterfly figure" - the main computation. Performed for each size
         // 2, 4, 8 ... in increasing order.
         for half_width in (0..).map(|e| 1 << e).take_while(|w| *w < self.size) {
-            for pos in (0..self.size).step_by(2 * half_width) {
+            let width = 2 * half_width;
+            let twiddle_step = self.size / width;
+            for pos in (0..self.size).step_by(width) {
                 for i in 0..half_width {
                     let l = array[pos + i];
                     let r = array[pos + half_width + i];
-                    let r = r * Complex::euler(-(i as Num) * PI / half_width as Num);
+                    // This expression is taken from the precomputed array instead:
+                    // Complex::euler(-(i as Num) * PI / half_width as Num)
+                    let r = r * self.twiddle_factors[i * twiddle_step];
                     array[pos + i] = l + r;
                     array[pos + half_width + i] = l - r;
                 }
